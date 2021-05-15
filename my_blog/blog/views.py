e3c973775da258cddcbb3e173039dd3efdd2b756
Oluwafemi <phemmylintry@gmail.com>
2021-05-15 22:22:32 +0100

    django templates

---
+++ my_blog/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    
+    posts = Post.objects.all()
+
+    context = {
+        "posts" : posts
+    }
+
+    return render(request, "blog/index.html", context)
